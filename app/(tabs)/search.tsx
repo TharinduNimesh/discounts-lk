087ad5d37ffd89babec85ee5c59658d44d09d478
sachith prasan <sachithprasan@gmail.com>
2024-10-20 21:21:31 +0530

    search input category

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,28 +1,124 @@
-import { Image, StyleSheet, Platform, View, Button } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Button,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  useWindowDimensions,
+  TextInput,
+} from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import Ellipse from "@/components/Ellipse";
+import React, { useState } from "react";
+import ProductCard from "@/components/ProductCard";
+import { useRouter } from "expo-router";
+import products from "@/scripts/products.json";
+import Input from "@/components/ButtonsAndInputs/UInput";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function HomeScreen() {
+export default function SearchScreen() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const categories = [
+    { name: "All" },
+    { name: "Grocery" },
+    { name: "Hotels" },
+    { name: "Rest" },
+    { name: "Fries" },
+    { name: "Pasta" },
+    // Add more categories as needed
+  ];
+
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <Ellipse />
+    <ThemedView>
+      <ThemedView className="bg-secondary">
+        <ThemedView className="p-6 mt-6">
+          <ThemedView className="flex-row items-center bg-secondary rounded-full p-3 border-2 border-[#CCCCCC]">
+            <AntDesign name="search1" size={24} color="#B2B2B2" />
+            <TextInput
+              className="ml-2 text-[#B2B2B2]  flex-1"
+              placeholder="Store, Product, etc."
+              // value={searchText}
+              // onChangeText={setSearchText}
+            />
+          </ThemedView>
+        </ThemedView>
 
-        <HelloWave />
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 0 }}
+        >
+          {categories.map((category, index) => (
+            <Pressable
+              key={index}
+              style={{
+                marginTop: 10,
+                marginBottom: 1,
+                alignItems: "center",
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                borderBottomWidth: 3,
+                backgroundColor:
+                  selectedCategory === category.name ? "#D9D9D94D" : "#ffffff",
+                borderBottomColor:
+                  selectedCategory === category.name ? "#000000" : "#A6A6A630",
+              }}
+              onPress={() => setSelectedCategory(category.name)}
+            >
+              <Text
+                style={{
+                  color:
+                    selectedCategory === category.name ? "#000000" : "#333",
+                  fontSize: 14,
+                }}
+              >
+                {category.name}
+              </Text>
+            </Pressable>
+          ))}
+        </ScrollView>
       </ThemedView>
-    </ParallaxScrollView>
+
+      <ScrollView className="bg-primary">
+        <ThemedView className="flex-1 px-2 mt-4 bg-primary mb-16">
+          {/* Discounts Section */}
+          <ThemedText className="text-2xl mb-3" type="subtitle">
+            Result Of “Dominos”
+          </ThemedText>
+
+          {/* Product List */}
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              product={product}
+              onPress={() =>
+                router.push({
+                  pathname: "/product_info",
+                  params: { product: JSON.stringify(product) },
+                })
+              }
+            />
+          ))}
+        </ThemedView>
+      </ScrollView>
+    </ThemedView>
   );
 }
 
